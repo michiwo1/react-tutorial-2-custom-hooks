117c2dc6d5d6afc3e32c1521ce87717039a451d3
michiwo <54099086+michiwo1@users.noreply.github.com>
2021-04-22 13:11:58 +0900

    カスタムフックの使い方

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { UserCard } from "./components/UserCard";
 import "./styles.css";
+import { useAllUsers } from "./hooks/useAllUsers";
 
 export default function App() {
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+  const onClickFetchUser = () => getUsers();
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? <p>テスト1</p> : loading ? <p>テスト2</p> : <p>テスト3</p>}
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </div>
   );
 }
---
+++ src/types/userProfile.ts
@@ -0,0 +1,6 @@
+export type UserProlife = {
+  id: number;
+  name: string;
+  email: string;
+  address: string;
+};
